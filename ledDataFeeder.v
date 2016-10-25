6fabfede197099087e8e585073130f9ae179cf7f
kmd178 <kmd178@gmail.com>
2016-10-25 23:20:08 +0300

    Part C - Text from Memorry & Button directed Text Rolling
    Created and initialized memory in the ledDataFeeder module itself.
    Finished  showing text to leds from alternating memory registers instead
    of directly assigned registers.
    Didnt debug.
    Didnt modifie anti-bounce module to work on the reset button itself,
    initialization problem.
    {Normally i use the reset signal to initialize my registers and my FSM.}

--- ledDataFeeder.v
+++ ledDataFeeder.v
@@ -8,6 +8,20 @@ module ledDataFeeder(
 );
 
 reg [3:0] SystemCounter;
+reg [3:0] message [0:7];
+reg [2:0] MemoryCounter;
+
+assign message[0]=1'h1;   //1
+assign message[1]=1'h4;   //4
+assign message[2]=1'h3;   //3
+assign message[3]=1'h5;   //5
+assign message[4]=1'ha;   //a
+assign message[5]=1'hb;   //b
+assign message[6]=1'hc;   //C
+assign message[7]=1'hd;   //d
+
+
+
 
 always @(posedge clk,reset)
 	begin
@@ -17,6 +31,7 @@ always @(posedge clk,reset)
 			an2 <= 1;
 			an3 <= 1;
 		  SystemCounter <= 4'b0 ;
+		  MemoryCounter <= 4'b0 ;
 		end
 		else begin
 		  SystemCounter <= SystemCounter + 4'b0001;
@@ -25,34 +40,34 @@ always @(posedge clk,reset)
 
 always @(*)
 case(SystemCounter) 
-  4'h0: begin 
-			loadCharLED = 4'b0100; 
-//				MemoryCounter= MemoryCounter+4'b1 ; //0
+  1'h0: begin 
+			loadCharLED = message[MemoryCounter]; 
+			MemoryCounter= MemoryCounter+4'b1 ; //0
 			end  
   //4'h1: LED = 7'b1001111; //1
-  4'h2: an0 = 0; //2
-  4'h3: an0 = 1; //3
-  4'h4: begin 
-			loadCharLED = 4'b0001; 
-//			MemoryCounter= MemoryCounter+4'b1 ; //4
+  1'h2: an3 = 0; //2
+  1'h3: an3 = 1; //3
+  1'h4: begin 
+			loadCharLED = message[MemoryCounter]; 
+			MemoryCounter= MemoryCounter+4'b1 ; //0
 		  end
   //4'h5: LED = 7'b0100100; //5
-  4'h6: an1 = 0; //6
-  4'h7: an1 = 1; //7 
-  4'h8: begin 
-			loadCharLED = 4'b0010; 
-//			MemoryCounter= MemoryCounter+4'b1 ; //8
+  1'h6: an2 = 0; //6
+  1'h7: an2 = 1; //7 
+  1'h8: begin 
+			loadCharLED = message[MemoryCounter]; 
+			MemoryCounter= MemoryCounter+4'b1 ; //0
 		  end
   //4'h9: LED = 7'b0000100; //9
-  4'ha: an2 = 0; //a
-  4'hb: an2 = 1; //b
-  4'hc: begin
-			loadCharLED = 4'b1101; 
-//			MemoryCounter= MemoryCounter+4'b1 ; //C
+  1'ha: an1 = 0; //a
+  1'hb: an1 = 1; //b
+  1'hc: begin
+			loadCharLED = message[MemoryCounter]; 
+			MemoryCounter= MemoryCounter+4'b1 ; //0
 		  end
   //4'hd: LED = 7'b1000010; //d
-  4'he: an3 = 0; //E
-  4'hf: an3 = 1; //F
+  1'he: an0 = 0; //E
+  1'hf: an0 = 1; //F
 endcase
 
 endmodule
